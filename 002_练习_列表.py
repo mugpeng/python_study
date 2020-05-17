4b3d2dd2bda9e5f2e63e352fda0bb67b0b7fcd5b
Peng <17098533pa11001@student.must.edu.mo>
2020-05-17 09:10:43 +0800

    DAY2

---
+++ 002_练习_列表.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+
+# 习题
+# 打印Apple:
+print(L[0][0])
+# 打印Python:
+print(L[1][1])
+# 打印Lisa:
+print(L[2][2])
